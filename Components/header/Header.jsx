6686cada763d1fca22e79c810bf752837d0f9d05
Protap Ghosh <protapg518@gmail.com>
2023-04-09 14:06:18 +0600

    added header component

---
+++ Components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='nav-div p-5'>
+            <nav className='navbar w-full max-w-[1200px] m-auto flex justify-between'>
+                <h1 className='text-2xl font-bold'>Knowledge Cafe</h1>
+                <img src="https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg" className='rounded-full' alt="" srcset="" />
+                {/* <img src="https://api.lorem.space/image/face?w=40&amp;amp;amp;amp;h=40" className='rounded-full' alt="" srcset="" /> */}
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
